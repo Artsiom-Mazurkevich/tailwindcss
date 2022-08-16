8185cd4ff4ea5c5a9b34af33807d7a19bc904ba8
Artsiom-Mazurkevich <patrol1995@yandex.ru>
2022-08-16 22:58:25 +0300

    first training tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        mainBGColor: '#0F172A'
+      }
+    },
+  },
+  plugins: [],
+}
